272f34c1d05a906e343986bb2552a5f918f0a974
Aleen <aleen42@vip.qq.com>
2023-02-09 17:42:37 +0800

    Support for GA V4

--- book/plugin.js
+++ book/plugin.js
@@ -1,17 +1,17 @@
-require(["gitbook"], function(gitbook) {
+require(["gitbook"], function (gitbook) {
     // Load analytics.js
-    gitbook.events.bind("start", function(e, config) {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    gitbook.events.bind("start", function (e, config) {
+        var script = document.createElement('script');
+        var cfg = config.ga, token = cfg.token;
+        script.src = 'https://www.googletagmanager.com/gtag/js?id=' + token;
+        script.onload = function () {
+            window.dataLayer = window.dataLayer || [];
 
-        var cfg = config.ga;
-        ga('create', cfg.token, cfg.configuration);
-    });
+            function gtag() {dataLayer.push(arguments);}
 
-    // Notify pageview
-    gitbook.events.bind("page.change", function() {
-        ga('send', 'pageview', window.location.pathname+window.location.search);
+            gtag('js', new Date());
+            gtag('config', token);
+        };
+        document.head.appendChild(script);
     });
 });
